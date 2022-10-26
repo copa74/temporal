24e5ec4adb8bbb6580f0ea842d00df2449b29f7f
wonhyeok.choi <yuanhe1995@naver.com>
2022-10-26 21:55:46 +0900

    won

--- src/main/jsx/App.jsx
+++ src/main/jsx/App.jsx
@@ -4,6 +4,7 @@ import  Header from  './Header.jsx'
 import  MainPage from  './MainPage.jsx'
 import  Qmenu from './Qmenu.jsx'
 
+// git test
 function App() {
   const [section, setSection] = useState("main")
 
